b964b1953bba21aef82c3b01ebc216a0749e548b
Ushen Wijayaratne <ushen05@gmail.com>
2025-03-19 14:23:44 +0000

    changed the app port and host

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ with app.app_context():
 
 if __name__ == '__main__':
     # This makes Flask accessible from other devices on the network
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=10000)
